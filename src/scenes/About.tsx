ae8725a8aed7fdc1d87370ac50e2723246e733c3
Dana Ng <ngdana1@gmail.com>
2023-08-26 00:09:10 -0700

    update about page

--- src/scenes/About.tsx
+++ src/scenes/About.tsx
@@ -1,5 +1,6 @@
 import { SceneValues } from "@/utils/GameTypes";
 import { BaseScene } from "./BaseScene";
+import { addTextToScene } from "@/utils/helperMethods";
 
 export default class About extends BaseScene {
   constructor() {
@@ -9,5 +10,80 @@ export default class About extends BaseScene {
 
   create() {
     this.createScene("About", "Menu", SceneValues.MenuScene);
+    this.generateAboutContent();
+  }
+
+  generateAboutContent() {
+    const centerX = this.cameras.main.width / 2;
+    const centerY = this.cameras.main.height / 2;
+
+    const container = this.add.container(centerX, centerY + 50);
+    const square = this.add.graphics();
+    square.fillStyle(0xffffff, 1);
+
+    const width = 400;
+    const height = 400;
+    const borderRadius = 20;
+
+    square.fillRoundedRect(
+      -width / 2,
+      -height / 2,
+      width,
+      height,
+      borderRadius
+    );
+    container.add(square);
+
+    const text = addTextToScene(
+      this,
+      " Lingua Hub is a language learning app using simple playful quizzes to help learners understand the basics of another language.",
+      container.width,
+      0.8
+    );
+    text.setY(square.y - 180);
+
+    const text2 = addTextToScene(
+      this,
+      " The languages available to try currently are Turkish, Hindi and Korean",
+      container.width,
+      0.8
+    );
+    text2.setY(-50);
+
+    const text3 = addTextToScene(
+      this,
+      "Please reach out if you have any questions!",
+      container.width,
+      0.8
+    );
+
+    text3.setY(-text2.y * 3 - 100);
+
+    const text4 = addTextToScene(
+      this,
+      "https://github.com/riceball1",
+      container.width,
+      0.8
+    );
+
+    text4.setY(-text2.y * 4 - 100);
+
+    const textContent = [text, text2, text3, text4];
+
+    textContent.forEach((textItem, index) => {
+      textItem.setFill("#000");
+      textItem.setBackgroundColor("transparent");
+      textItem.setWordWrapWidth(width);
+      container.add(textItem);
+      textItem.setAlign("justify");
+
+      if(index === 3) {
+        textItem.setInteractive()
+        textItem.setFill('#00BFFF')
+        textItem.on('pointerup', () => {
+          window.open('https://github.com/riceball1',"_blank" )
+        })
+      }
+    });
   }
 }
--- src/utils/helperMethods.ts
+++ src/utils/helperMethods.ts
@@ -1,6 +1,12 @@
 import Phaser from "phaser";
+import { SceneValues } from "./GameTypes";
 
-export function addTextToScene(scene: Phaser.Scene, text: string, cameraWidth: number, scale: number = 1) {
+export function addTextToScene(
+  scene: Phaser.Scene,
+  text: string,
+  cameraWidth: number,
+  scale: number = 1
+) {
   const centerX = cameraWidth / 2;
 
   const textStyle = {
@@ -9,32 +15,68 @@ export function addTextToScene(scene: Phaser.Scene, text: string, cameraWidth: n
     backgroundColor: "#000000",
     padding: {
       x: 10,
-      y: 5
-    }
+      y: 5,
+    },
   };
 
-  const textObj = scene.add.text(centerX, 20, text, textStyle)
-    .setOrigin(0.5, 0);
+  const textObj = scene.add.text(
+    centerX,
+    20,
+    text,
+    textStyle
+  );
 
-    textObj.setScale(scale)
-
+  textObj.setOrigin(0.5, 0)
+  textObj.setScale(scale);
 
   return textObj;
 }
 
-export function addButtonToScene(scene: Phaser.Scene, text: string, x: number, y: number) {
-    const button = scene.add.text(x, y + 150, text, {
+export function addButtonToScene(
+  scene: Phaser.Scene,
+  text: string,
+  x: number,
+  y: number,
+  scale: number = 1
+) {
+  const container = scene.add.container(x, y + 150);
+
+  const buttonBackground = scene.add.graphics();
+  buttonBackground.fillStyle(0xaaffaa, 1);
+
+  const width = 100;
+  const height = 60;
+  const borderRadius = 20;
+
+  buttonBackground.fillRoundedRect(
+    -width / 2,
+    -height / 2,
+    width,
+    height,
+    borderRadius
+  );
+
+  container.add(buttonBackground);
+
+  const buttonText = scene.add
+    .text(0, 0, text, {
       fontSize: "20px",
       color: "#000",
-      backgroundColor: '#AAFFAA',
-      padding: {
-        x: 10,
-        y: 5,
-      },
     })
-      .setOrigin(0.5)
-      .setScale(1.5)
-      .setInteractive();
-  
-    return button;
-  }
+    .setOrigin(0.5)
+    .setScale(scale);
+
+  container.add(buttonText);
+
+  container.setInteractive(
+    new Phaser.Geom.Rectangle(-width / 2, -height / 2, width, height),
+    Phaser.Geom.Rectangle.Contains
+  );
+
+  container.on("pointerup", () => {
+    console.log("Menu button clicked");
+    scene.scene.start(SceneValues.MenuScene);
+  });
+
+  return container;
+}
